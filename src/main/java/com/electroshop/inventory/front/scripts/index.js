969a768c2e64ea1dec33857f01ce4b38d94cc2cb
Joao Vitor Hess Nardes <jvhn2006@gmail.com>
2024-07-09 20:50:42 -0300

    Js add and main

--- src/main/java/com/electroshop/inventory/front/scripts/index.js
+++ src/main/java/com/electroshop/inventory/front/scripts/index.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-    $("#toggleButton").click(function(){
-        $("#conteudo").toggle(); // Mostra ou esconde o conteúdo
+
+$(document).ready(function () {
+    $("#backToggle").click(function () {
+        $("#mainContent").html(`
+            <h1>Inicio</h1>
+        `);
+    });
+});
+
+$(document).ready(function () {
+    $("#toggleButtonToViewStock").click(function () {
+        $("#mainContent").html(`
+            <div class="container">
+                <h1 class="text-center"> ViewStock </h1>
+            </div>
+        `);
+    });
+});
+
+$(document).ready(function () {
+    $("#toggleButtonToAddOrRemoveProduct").click(function () {
+        $("#mainContent").html(`
+            <div class="container">
+                <h1 class="text-center"> Remove or Add Products 
+        `);
     });
 });
